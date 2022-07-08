cd9123646e6a993458736f8444d7717aeb82881f
Евгений <nashinvo@gmail.com>
2022-07-08 16:19:36 +0300

    Added hash algorithm select and the ability to get the hash of a file

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,6 +4,7 @@ var CAPICOM_MY_STORE = "My";
 var CAPICOM_STORE_OPEN_MAXIMUM_ALLOWED = 2;
 var CAPICOM_CERTIFICATE_FIND_SUBJECT_NAME = 1;
 var CADESCOM_BASE64_TO_BINARY = 1;
+var CADESCOM_HASH_ALGORITHM = 100;
 
 function run() {
     cadesplugin.async_spawn(function* (args) {
@@ -36,7 +37,7 @@ function run() {
                 var CADESCOM_HASH_ALGORITHM_CP_GOST_3411 = 100
                 // Создаем объект CAdESCOM.HashedData
                 var oHashedData = yield cadesplugin.CreateObjectAsync("CAdESCOM.HashedData");
-                oHashedData.propset_Algorithm(CADESCOM_HASH_ALGORITHM_CP_GOST_3411);
+                oHashedData.propset_Algorithm(CADESCOM_HASH_ALGORITHM);
                 oHashedData.propset_DataEncoding(CADESCOM_BASE64_TO_BINARY);
                 yield oHashedData.Hash(sBase64Data);
                 var hashValue = yield oHashedData.Value;
@@ -113,11 +114,18 @@ function downloadSignature() {
         return;
     }
 
-    let signedFile = document.getElementById("uploadFile").files[0];
-    const file = signedFile.name;
-    const lastDot = file.lastIndexOf('.');
-    const fileName = file.substring(0, lastDot);
-    const fileExtension = file.substring(lastDot + 1);
+    let fileName, fileExtension;
+    if (document.getElementById("uploadFile").files.length != 0) {
+        let signedFile = document.getElementById("uploadFile").files[0];
+        const file = signedFile.name;
+        const lastDot = file.lastIndexOf('.');
+        fileName = file.substring(0, lastDot);
+        fileExtension = file.substring(lastDot + 1);
+    }
+    else {
+        fileName = "example";
+        fileExtension = "ext";
+    }
 
     let signatureFile = new Blob([signature], { type: 'text/html' });
     let a = document.createElement("a");
@@ -140,12 +148,10 @@ function additionSelectOptions() {
             try {
                 cert = yield certsList.Item(i);
                 var str = yield cert.SubjectName;
-                console.log("CERT NAME: " + str);
                 var resultString = str.match(regExp)[0];
                 var firstcomma = resultString.indexOf(',');
                 if (firstcomma == -1) firstcomma = resultString.length - 1;
                 resultString = resultString.slice(3, firstcomma);
-                console.log("RESULT: " + resultString);
                 $('#CertNameSelect').prepend($('<option>' + resultString + '</option>'));
             }
             catch (ex) {
@@ -156,6 +162,49 @@ function additionSelectOptions() {
     });
 }
 
+function getHash() {
+    cadesplugin.async_spawn(function* (args) {
+        // Проверяем, работает ли File API
+        if (window.FileReader) {
+            // Браузер поддерживает File API.
+        } else {
+            alert('The File APIs are not fully supported in this browser.');
+        }
+        if (0 === document.getElementById("uploadFile").files.length) {
+            alert("Не выбран файл для подписи!");
+            return;
+        }
+        var oFile = document.getElementById("uploadFile").files[0];
+        var oFReader = new FileReader();
+
+        if (typeof (oFReader.readAsDataURL) != "function") {
+            alert("Method readAsDataURL() is not supported in FileReader.");
+            return;
+        }
+
+        oFReader.readAsDataURL(oFile);
+
+        oFReader.onload = function (oFREvent) {
+            cadesplugin.async_spawn(function* (args) {
+                var header = ";base64,";
+                var sFileData = oFREvent.target.result;
+                var sBase64Data = sFileData.substr(sFileData.indexOf(header) + header.length);
+
+                // Создаем объект CAdESCOM.HashedData
+                var oHashedData = yield cadesplugin.CreateObjectAsync("CAdESCOM.HashedData");
+                oHashedData.propset_Algorithm(CADESCOM_HASH_ALGORITHM);
+                oHashedData.propset_DataEncoding(CADESCOM_BASE64_TO_BINARY);
+                yield oHashedData.Hash(sBase64Data);
+                var hashValue = yield oHashedData.Value;
+                document.getElementById("hash").innerHTML = hashValue;
+            });
+        };
+    });
+}
+
+function setHashAlgorithm() {
+    CADESCOM_HASH_ALGORITHM = document.getElementById("HashAlgorithmSelect").value;
+}
 
 $(document).ready(function () {
     var inputArea = document.getElementById('uploadFile');
